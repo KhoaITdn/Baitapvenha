3087f03a668b31e8b966dc2de3003a4af5f821a5
KhoaITdn <166222072+KhoaITdn@users.noreply.github.com>
2024-05-08 13:41:18 +0700

    Delete bai4.js

--- bai4.js
+++
@@ -1,6 +0,0 @@
-<script>
-  const radius = parseFloat(prompt("Nhập bán kính hình tròn:"));
-  const circumference = 2 * Math.PI * radius;
-
-  document.write("Chu vi hình tròn: " + circumference);
-</script>
